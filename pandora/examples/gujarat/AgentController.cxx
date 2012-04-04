728611ed97ac89f86e5316dde7e23bd4e4f8ce55
Miquel Ramírez <miquel.ramirez@gmail.com>
2012-04-04 21:15:18 +0200

    + ADDED: AgentController base class

---
+++ pandora/examples/gujarat/AgentController.cxx
@@ -0,0 +1,15 @@
+#include "AgentController.hxx"
+
+namespace Gujarat
+{
+
+AgentController::AgentController( GujaratAgent* a )
+	: _agent( a )
+{
+}
+
+AgentController::~AgentController()
+{
+}
+
+}
