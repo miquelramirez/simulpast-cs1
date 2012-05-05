af068d804a9cb9f44e663ea8081fba05d4c997cb
Miquel Ramírez <miquel.ramirez@gmail.com>
2012-05-05 13:30:05 +0200

    + FIXED: Routines generating actions for the MDP controllers and other controllers are now unified + ADDED: Asserts to check post-condition of generatePossibleActions()

--- pandora/examples/gujarat/MoveHomeAction.cxx
+++ pandora/examples/gujarat/MoveHomeAction.cxx
@@ -32,14 +32,18 @@ void	MoveHomeAction::describe( std::ostream& os ) const
 	os << "move_home( " << _newHomeLoc._x << ", " << _newHomeLoc._y << ")";
 }
 
+void	MoveHomeAction::generatePossibleActions( const GujaratAgent& agent, std::vector< MoveHomeAction* >& actions )
+{
+	generatePossibleActions( agent, agent.getPosition(), actions );
+}
 
 void MoveHomeAction::generatePossibleActions( const GujaratAgent& agent,
-						const Engine::Point2D<int>& agentPos,
+						const Engine::Point2D<int>& agentPos, 
 						std::vector< MoveHomeAction* >& actions )
 {
 	// Put a BOX around the home range.	
-	int boxOriginX = agent.getPosition()._x - agent.getHomeMobilityRange();
-	int boxOriginY = agent.getPosition()._y - agent.getHomeMobilityRange();
+	int boxOriginX = agentPos._x - agent.getHomeMobilityRange();
+	int boxOriginY = agentPos._y - agent.getHomeMobilityRange();
 	int boxSizeX = 2*agent.getHomeMobilityRange()+1;
 	int boxSizeY = 2*agent.getHomeMobilityRange()+1;
 	Engine::Point2D<int> boxOrigin(boxOriginX, boxOriginY);	
@@ -55,6 +59,8 @@ void MoveHomeAction::generatePossibleActions( const GujaratAgent& agent,
 	const SettlementAreas * settlementAreas = world->getSettlementAreas();
 	std::vector<int> candidates(0);
 	settlementAreas->intersectionFilter(homeBox,candidates);
+
+	assert( !candidates.empty() );
 	
 	// Select Areas with maximum score.
 	// Sort candidates following scores	
@@ -70,8 +76,7 @@ void MoveHomeAction::generatePossibleActions( const GujaratAgent& agent,
 		// ATM: above you will find settlementAreas->intersectionFilter method call. This ensures any
 		// item inside candidates vector has intersection with homeBox. So the 'if' is not
 		// needed, we just recompute the intersection to use it.
-		bool foo = homeBox.intersection(selectedArea, intersection);
-		//if ( !homeBox.intersection(selectedArea, intersection) ) continue;
+		homeBox.intersection(selectedArea, intersection);
 		
 		// Extract one random dune cell which is inside the homeRange and inside the selected area.
 		
@@ -84,7 +89,7 @@ void MoveHomeAction::generatePossibleActions( const GujaratAgent& agent,
 			for (index._y = intersection._origin._y; index._y < intersection._origin._y+intersection._size._y; index._y++)			
 			{
 				if ((world->getValue("soils",index) == DUNE) 
-					&& (agent.getPosition().distance(index) <= (double)agent.getHomeMobilityRange()))
+					&& (agentPos.distance(index) <= (double)agent.getHomeMobilityRange()))
 				{
 					countDunes++;
 					dunes.push_back(index);
@@ -93,96 +98,14 @@ void MoveHomeAction::generatePossibleActions( const GujaratAgent& agent,
 		}		
 
 		if ( dunes.empty() ) 
+		{
+			std::cout << "No dunes in candidate area!" << std::endl;
 			continue;
+		}
 		uint32_t diceSelectOneRandomDune = world->getStatistics().getUniformDistValue(0, dunes.size()-1);
 		actions.push_back( new MoveHomeAction( dunes[ diceSelectOneRandomDune ] ) );
 	}
-	candidates.clear();
-}
-
-void MoveHomeAction::generatePossibleActions( 	GujaratAgent& agent,
-						std::vector< MoveHomeAction* >& actions )
-{
-	// Put a BOX around the home range.	
-	int boxOriginX = agent.getPosition()._x - agent.getHomeMobilityRange();
-	int boxOriginY = agent.getPosition()._y - agent.getHomeMobilityRange();
-	int boxSizeX = 2*agent.getHomeMobilityRange()+1;
-	int boxSizeY = 2*agent.getHomeMobilityRange()+1;
-	Engine::Point2D<int> boxOrigin(boxOriginX, boxOriginY);	
-	Engine::Point2D<int> boxSize(boxSizeX,boxSizeY);	
-	Engine::Rectangle<int> unTrimmedHomeBox(boxOrigin,boxSize); 
-	Engine::Rectangle<int> homeBox;
-	//TODO look out sectors, MPI regions, etc... Decide getBoundaries? or getOverlapBoundaries?
-	// MRJ: As I understand it, this should be getOverlapBoundaries(), very much as it was before
-	unTrimmedHomeBox.intersection(agent.getWorld()->getOverlapBoundaries(),homeBox);
-	
-	// Retrieve the areas that have intersection non zero with homeBox
-	GujaratWorld * world              = (GujaratWorld *)agent.getWorld();
-	SettlementAreas * settlementAreas = world->getSettlementAreas();
-	std::vector<int> candidates(0);
-	settlementAreas->intersectionFilter(homeBox,candidates);
-	
-	// Select Areas with maximum score.
-	// Sort candidates following scores	
-	std::make_heap(candidates.begin(),candidates.end(),Gujarat::compareSettlementAreas(settlementAreas));		
-	
-	int i = 0;
-	
-	for ( unsigned i = 0; i < candidates.size(); i++ )
-	{
-		Engine::Rectangle<int> selectedArea = settlementAreas->getAreaById(candidates[i]);
-		Engine::Rectangle<int> intersection;
-		// MRJ: If the selected area is outside of the "homeBox" then why caring about it all?
-		// ATM: above you will find settlementAreas->intersectionFilter method call. This ensures any
-		// item inside candidates vector has intersection with homeBox. So the 'if' is not
-		// needed, we just recompute the intersection to use it.
-		bool foo = homeBox.intersection(selectedArea, intersection);
-		//if ( !homeBox.intersection(selectedArea, intersection) ) continue;
-		
-		// Extract one random dune cell which is inside the homeRange and inside the selected area.
-		
-		// count dunes from candidate area "i", 'selectedArea' variable
-		int countDunes = 0;
-		Engine::Point2D<int> index;
-		for (index._x = intersection._origin._x; index._x < intersection._origin._x+intersection._size._x; index._x++)			
-		{
-			for (index._y = intersection._origin._y; index._y < intersection._origin._y+intersection._size._y; index._y++)			
-			{
-				if ((world->getValue("soils",index) == DUNE) 
-					&& (agent.getPosition().distance(index) <= (double)agent.getHomeMobilityRange()))
-				{
-					countDunes++;
-				}
-			}
-		}		
-		
-		bool foundDune = false;
-		// pick one dune at random		
-		uint32_t diceSelectOneRandomDune = world->getStatistics().getUniformDistValue(0, countDunes);
-		for (index._x = intersection._origin._x; 
-			 index._x < intersection._origin._x+intersection._size._x;
-			 index._x++)			
-		{
-			for (index._y = intersection._origin._y; 
-				 index._y < intersection._origin._y+intersection._size._y;
-				 index._y++)				
-			{
-				if (world->getValue("soils",index) == DUNE
-					&& (agent.getPosition().distance(index) <= (double)agent.getHomeMobilityRange())
-					&& diceSelectOneRandomDune == 0.0)
-				{	
-					actions.push_back( new MoveHomeAction( index ) );	
-					foundDune   = true;
-					break;
-				}
-				diceSelectOneRandomDune--;
-			}
-			if (foundDune)	
-			{
-				break;
-			}
-		}		
-	}
+	assert( !actions.empty() );
 	candidates.clear();
 }
 
