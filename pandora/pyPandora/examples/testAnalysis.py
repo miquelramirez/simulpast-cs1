b62da5dff663f92fdbb86e4ccf5924e67db2d9e1
Xavier Rubio-Campillo <xrubio@gmail.com>
2012-05-24 16:26:35 +0200

    + Added python interface to Analysis

--- pandora/pyPandora/examples/testAnalysis.py
+++ pandora/pyPandora/examples/testAnalysis.py
@@ -3,23 +3,22 @@
 import sys
 sys.path.append('..')
 sys.path.append('../../')
-from pyPandora import SimulationRecord, AgentMean, AgentSum, AgentNum, RasterMean, RasterSum, AgentResults, RasterResults
+from pyPandora import SimulationRecord, AgentResults, AgentMean, AgentSum, AgentNum, RasterMean, RasterSum, RasterResults
 
 record = SimulationRecord()
 record.loadHDF5('data_test/gujarat.h5', 1, 1)
 
 agentResults = AgentResults(record, 'agents.csv', 'HunterGatherer', ';')
-foo = AgentNum()
-agentResults.addAnalysis(foo)
-#agentResults.addAnalysis(AgentMean('children'))
-#agentResults.addAnalysis(AgentSum('children'))
-#agentResults.addAnalysis(AgentMean('collected resources'))
+agentResults.addAnalysis(AgentNum())
+agentResults.addAnalysis(AgentMean('children'))
+agentResults.addAnalysis(AgentSum('children'))
+agentResults.addAnalysis(AgentMean('collected resources'))
 
 agentResults.compute()
 
-#rasterResults = RasterResults(record, 'resources.csv', 'resources')
-#rasterResults.addAnalysis(RasterMean)
-#rasterResults.addAnalysis(RasterSum)
+rasterResults = RasterResults(record, 'resources.csv', 'resources', ';')
+rasterResults.addAnalysis(RasterMean())
+rasterResults.addAnalysis(RasterSum())
 
-#rasterResults.compute()
+rasterResults.compute()
 
